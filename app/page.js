ca409fe579bf0f6ffa64644a878e885a17c71798
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-02-27 09:23:05 +0600

    added hero and about section

--- app/page.js
+++ app/page.js
@@ -2,10 +2,14 @@ import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
-  <div className='h-[100vh] bg-primary-black overflow-hidden'>
+  <div className=' bg-primary-black overflow-hidden'>
     <Navbar />
     <Hero />
-    <About />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
+    </div>
     <Explore />
     <GetStarted />
     <WhatsNew />
