a5c3860f7fb1861284d45e6078d868761a0b86fa
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-02-28 11:48:22 +0600

    complete full project

---
+++ _static/_next/static/j7o4q_sudEIqqxJnkjdWD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/566-5222e61c8f36b6bf.js","static/chunks/pages/index-5251943a4143ef51.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
